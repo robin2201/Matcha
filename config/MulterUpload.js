0a869000efd179b105d4ff11313ee12a13429e7f
robin berthier <robin@pc36.home>
2017-02-27 09:08:09 +0100

    Upload Pics function Add in MulterUpload

--- config/MulterUpload.js
+++ config/MulterUpload.js
@@ -2,20 +2,37 @@
  * Created by robin on 2/27/17.
  */
 const multer = require('multer')
+let path = require('path')
+let fs = require('fs')
 
 let storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        if(file.mimetype !== 'image/jpeg')
-            return callback('Sorry this Pic isn\'t a pic')
-        console.log(file)
-        callback(null, './uploads')
+    destination: (req, file, cb) => {
+        try {
+            fs.accessSync('./uploads')
+        } catch (e) {
+            fs.mkdirSync('./uploads')
+        }
+        cb(null, './uploads')
     },
-    filename: (req, file, callback) => {
-        callback(null, file.fieldname + '-' + Date.now())
+    limits: {
+        files: 1,
+        fileSize: 1024 * 1024
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
-});
+})
 
-let upload = multer({storage: storage}).single('image')
+let upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        let ext = path.extname(file.originalname);
+        if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+            return cb(new Error('Only images are allowed'))
+        }
+        cb(null, true)
+    }
+}).single('image')
 
 
 module.exports = upload;
