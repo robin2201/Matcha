98ff258b7fcb10e1fd8dbd57b27b11c0e74ad6a4
robin <rberthie@student.42.fr>
2017-03-09 14:44:03 +0100

    Modif

--- controllers/UserProfile.js
+++ controllers/UserProfile.js
@@ -39,6 +39,7 @@ module.exports = {
         let email = req.body.email
         let id = req.session.userId
         let user = req.session.user
+        
         const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         if(regex.test(email)){
             mongoUtil.connectToServer((err) => {
