bb3d67c58bd053ada78b945638a7b83b674255cd
robin <rberthie@student.42.fr>
2017-03-09 14:52:23 +0100

    Rm user auth in mail

--- config/mail.js
+++ config/mail.js
@@ -7,8 +7,7 @@ const transporter = nodemailer.createTransport({
     host: 'smtp.mailtrap.io',
     port: 2525,
     auth: {
-        user: '33c9502726c803',
-        pass: '3261b4bf3cd7a2'
+
     }
 });
 
