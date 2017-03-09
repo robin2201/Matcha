107f1d97091ff84423497a59c6bb2ca63f9a3a19
robin <rberthie@student.42.fr>
2017-03-09 14:45:50 +0100

    Modif UserProfile

--- config/emailTools.js
+++ config/emailTools.js
@@ -1,3 +1,10 @@
 /**
  * Created by robin on 3/9/17.
  */
+const transporter = require('../config/mail')
+
+module.exports = {
+    SendMail: (req, res, mess) => {
+
+    }
+}
--- config/mail.js
+++ config/mail.js
@@ -1,13 +1,14 @@
 /**
  * Created by robin on 2/21/17.
  */
-const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer')
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.mailtrap.io',
     port: 2525,
     auth: {
-
+        user: '33c9502726c803',
+        pass: '3261b4bf3cd7a2'
     }
 });
 
