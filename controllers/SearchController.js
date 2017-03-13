3fadbefb3b6b5dc9c021347ed2de4ce92d4dcdc3
robin <rberthie@student.42.fr>
2017-03-13 18:07:42 +0100

    Add Search templating engine

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -8,28 +8,20 @@ const objectId = require('mongodb').ObjectID
 module.exports = {
     SearchByNickname: (req, res) => {
         let user = req.session.user
-        console.log(req.body)
-        let searchU = req.body.search
-        console.log(searchU)
         mongoUtil.connectToServer((err) => {
             if (err) return res.sendStatus(500)
             let dbUser = mongoUtil.getDb().collection('Users')
-            let db = mongoUtil.getDb()
-            let findUser = {"location.region":"Île-de-France"}
-            let dbTest = db.collection('Users').find(findUser,
-                (err, result, cursor) => {
-                console.log(result)
-                    console.log(cursor)
-                })
-            console.log(dbTest)
-            //let mydc = dbTest.hasNext() ? dbTest.next() : null
-            //if(mydc){
-             //   console.log(mydc)
-            //}
-            req.session.user = user
-        })
+            let UsersSearch = []
+            dbUser.find({}).each((err, result) => {
+                if (result) {
+                    UsersSearch.push(result)
+                }
+                console.log(UsersSearch)
+                req.session.user = user
+                res.render('gallery', {users: UsersSearch})
+            })
 
-        res.render('home')
+        })
     },
     SearchByLocation: (req, res) => {
 
